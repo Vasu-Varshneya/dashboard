d18fa91d9254e0be7df323d81b3a37411a652902
Vasu <148642918+Vasu-Varshneya@users.noreply.github.com>
2024-10-16 19:32:16 +0530

    Update usermodel.js

--- backend/models/usermodel.js
+++ backend/models/usermodel.js
@@ -1,6 +1,11 @@
 
+
 var mongoose = require("mongoose")
-mongoose.connect("mongodb+srv://vasuvarshney26:vasu28@cluster0.1azvq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/")
+const func =()=>{
+    mongoose.connect("mongodb+srv://vasuvarshney26:vasu28@cluster0.1azvq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    console.log("Database connected")
+}
+func();
 const UserSchema = new mongoose.Schema({
     Name :{
         type:String,
