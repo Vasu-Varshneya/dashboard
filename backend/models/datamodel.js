ee7ec4efa3a33727e268450314cad51d877233d0
Vasu <148642918+Vasu-Varshneya@users.noreply.github.com>
2024-10-16 19:32:35 +0530

    Update datamodel.js

--- backend/models/datamodel.js
+++ backend/models/datamodel.js
@@ -1,5 +1,10 @@
 var mongoose = require("mongoose")
-mongoose.connect("mongodb+srv://vasuvarshney26:vasu28@cluster0.1azvq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/")
+
+const func =()=>{
+    mongoose.connect("mongodb+srv://vasuvarshney26:vasu28@cluster0.1azvq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    console.log("Database connected")
+}
+func();
 const DataSchema =new mongoose.Schema({
     x:Number,
     y:Number,
